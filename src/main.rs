d4e97536f764b82d11929bf0aa676691b50c2634
Ralph Giles <giles@thaumas.net>
2018-12-08 19:18:55 -0800

    Quick and dirty transliteration -> MdC.
    Uses a stack of replace() calls which is easy, but probably quite slow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Text conversion for Ancient Egyptian.
+
 fn main() {
-    println!("Hello, world!");
+    for arg in std::env::args().skip(1) {
+        let mdc = arg.replace("ꜣ", "A")
+                     .replace("ꜥ", "a")
+                     .replace("š", "S")
+                     .replace("ḥ", "H")
+                     .replace("ḫ", "x")
+                     .replace("ẖ", "X")
+                     .replace("ṯ", "T")
+                     .replace("ḏ", "D");
+        println!("arg: {} -> {}", arg, mdc);
+    }
+    println!("That's all!");
 }
