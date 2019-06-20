91dd3f23824dae63c9e510438db704e24e3dad57
Ralph Giles <giles@thaumas.net>
2019-06-20 16:54:00 -0700

    Move character map to an array and iterate over it.
    This makes it possible to flip the direction of the
    conversion.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@
 
 fn main() {
     for arg in std::env::args().skip(1) {
-        let mdc = arg.replace("ꜣ", "A")
-                     .replace("ꜥ", "a")
-                     .replace("š", "S")
-                     .replace("ḥ", "H")
-                     .replace("ḫ", "x")
-                     .replace("ẖ", "X")
-                     .replace("ṯ", "T")
-                     .replace("ḏ", "D");
+        let mut mdc = arg;
+        for (unicode, ascii) in [ ("ꜣ", "A"),
+                                ("ꜥ", "a"),
+                                ("š", "S"),
+                                ("ḥ", "H"),
+                                ("ḫ", "x"),
+                                ("ẖ", "X"),
+                                ("ṯ", "T"),
+                                ("ḏ", "D"),
+                                //("ḳ", "q"),
+                              ].iter() {
+            //mdc = mdc.replace(unicode, ascii);
+            mdc = mdc.replace(ascii, unicode);
+        }
         println!("{}", mdc);
     }
 }
